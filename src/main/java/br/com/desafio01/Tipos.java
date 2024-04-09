dac73ba5c2dba66d15a4d0af48adfd129caf7526
Vinicius Fillos <vinifillos@gmail.com>
2024-04-09 12:33:48 -0300

    feat: package desafio01 e classe Tipos implementada

---
+++ src/main/java/br/com/desafio01/Tipos.java
@@ -0,0 +1,15 @@
+package br.com.desafio01;
+
+import java.lang.reflect.Field;
+
+public class Tipos {
+
+    byte aByte;
+    short aShort;
+    int aInt;
+    long aLong;
+    float aFloat;
+    double aDouble;
+    boolean aBoolean;
+    char aChar;
+}
